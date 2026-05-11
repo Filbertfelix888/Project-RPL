cdb432efa0dbb21203f0d881ac5640323a96a844
Filbertfelix888 <filbert.felix888@gmail.com>
2026-05-11 21:39:17 +0700

    update navbar

--- src/components/layouts/SidebarLayout/Navbar/Navbar.jsx
+++ src/components/layouts/SidebarLayout/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import { AccountCircle } from '@mui/icons-material';
-import { Box, Stack } from '@mui/material';
+import { Box, Stack, Typography} from '@mui/material';
 import { grey } from '@mui/material/colors';
 
 import Dropdown from '../../../ui/Dropdown';
+
+import logo from '@/assets/PM_Logo.png';
 
 import session from '@/utils/session';
 import { useNavigate } from 'react-router';
@@ -15,7 +17,8 @@ const Navbar = () => {
   return (
     <Box
       sx={{
-        padding: 1,
+        paddingY: 0.5,
+        paddingX: 1,
         borderBottom: `1px solid ${grey[300]}`,
         background: '#ffffff',
         position: 'fixed',
@@ -25,7 +28,17 @@ const Navbar = () => {
         zIndex: 1100,
       }}
     >
-      <Stack justifyContent={'center'} alignItems={'flex-end'} paddingX={1}>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Stack direction="row" alignItems="center">
+          <Box component="img" src={logo} alt="logo" sx={{ width: 36, height: 36, mr: 1 }} />
+          <Typography variant="h3" sx={{
+            fontWeight: 500,
+            fontFamily: 'Calibri'
+          }}
+          >
+            PROJECT MANAGEMENT
+          </Typography>
+        </Stack>
         <Dropdown
           icon={<AccountCircle />}
           options={[
